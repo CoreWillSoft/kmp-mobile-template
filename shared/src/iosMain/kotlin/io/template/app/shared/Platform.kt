4452be3c66b3987bac3028788725bbf69a86f9d8
anton <anton.khorunzhyi@corewillsoft.com>
2023-01-18 10:15:49 +0200

    prepare commonMain dependencies

--- shared/src/iosMain/kotlin/io/template/app/shared/Platform.kt
+++ shared/src/iosMain/kotlin/io/template/app/shared/Platform.kt
@@ -2,7 +2,7 @@ package io.template.app.shared
 
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
+class IOSPlatform : Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
