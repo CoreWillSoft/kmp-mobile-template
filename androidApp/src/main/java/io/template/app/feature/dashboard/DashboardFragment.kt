1bd196d514cee896afff2eed24f3cdfd55ec48b9
anton <anton.khorunzhyi@corewillsoft.com>
2023-01-19 15:08:13 +0200

    optimize imports

--- androidApp/src/main/java/io/template/app/feature/dashboard/DashboardFragment.kt
+++ androidApp/src/main/java/io/template/app/feature/dashboard/DashboardFragment.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import android.view.View
 import io.template.app.R
 import io.template.app.common.ui.BaseFragment
-import io.template.app.common.ui.viewLifecycleLazy
-import io.template.app.databinding.DashboardLayoutBinding
-import io.template.library.android.NotificationUtil
 
 class DashboardFragment : BaseFragment(R.layout.dashboard_layout) {
 
