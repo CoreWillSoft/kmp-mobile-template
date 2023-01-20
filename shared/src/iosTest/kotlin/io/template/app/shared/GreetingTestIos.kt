36715bb6ee97787d80dc861049a946fd32526fec
anton <anton.khorunzhyi@corewillsoft.com>
2023-01-20 10:04:52 +0200

    added ios sample test

--- shared/src/iosTest/kotlin/io/template/app/shared/GreetingTestIos.kt
+++ shared/src/iosTest/kotlin/io/template/app/shared/GreetingTestIos.kt
@@ -1,3 +1,12 @@
 package io.template.app.shared
 
-class GreetingTestsIos
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class GreetingTestsIos {
+
+    @Test
+    fun testHello() {
+        assertTrue("iOS" in Greeting().greet())
+    }
+}
